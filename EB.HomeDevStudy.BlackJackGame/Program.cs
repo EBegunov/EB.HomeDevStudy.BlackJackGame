721d9e7f87a807463242c35ba1b00f49fad191c0
Vizeo <77226226+EBegunov@users.noreply.github.com>
2021-01-16 17:03:26 +0200

    Добавьте файлы проекта.

---
+++ EB.HomeDevStudy.BlackJackGame/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EB.HomeDevStudy.Main
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
